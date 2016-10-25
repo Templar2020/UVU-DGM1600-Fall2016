6c5700be9f4e02168dc06187b3d58ccc9944ab49
Templar2020 <preston.powell@uvu.edu>
2016-10-24 18:09:53 -0600

    Create move3D.cs
    10/24/16

---
+++ move3D.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Move Forward
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+	}
+}
