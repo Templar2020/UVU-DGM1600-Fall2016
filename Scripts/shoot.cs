2d11fb54498ec86a6466d50b4daa1f48a765c0a5
Templar2020 <preston.powell@uvu.edu>
2016-11-02 15:37:02 -0600

    Add files via upload

---
+++ Scripts/shoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+
+public Rigidbody projectile;
+public Transform Spawnpoint;
+public int shootSpeed;
+
+// Use this for initialization
+void Start () {
+
+}
+
+// Update is called once per frame
+void Update () {
+	if(Input.GetButtonDown("Fire1")){
+		Rigidbody clone;
+		clone = (Rigidbody)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+		clone.velocity = Spawnpoint.TransformDirection (Vector3.forward*shootSpeed);
+	}
+}
+}
